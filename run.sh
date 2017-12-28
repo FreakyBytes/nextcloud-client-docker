a72ed2f2f7f705c1b800e2993e365e4bfbaedfe6
Martin Peters <martin@freakybytes.net>
2017-12-28 21:57:51 +0100

    got rid of dynamic user add again

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/ash
 
-# check if the sync user exists
-id -u $USER || (addgroup -g $USER_GID $USER && adduser -D -H -u $USER_UID $USER)
-
 # ensure, that the sync dir exists and is owned by the user
 [ -d $NC_SYNC_DIR ] || mkdir -p $NC_SYNC_DIR
-chown $USER:$USER $NC_SOURCE_DIR
+chown -R $USER:$USER $NC_SOURCE_DIR
 
 su $USER -c 'nextcloudcmd $( [ $NC_SILENT == true ] && echo "--silent" ) $( [ $NC_TRUST_CERT == true ] && echo "--trust" ) --non-interactive -u $NC_USER -p $NC_PASS $NC_SOURCE_DIR $NC_URL'
