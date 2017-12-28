ef0ba34f42fd3fa08761ffbf96c984c6ff1c2da5
Martin Peters <martin@freakybytes.net>
2017-12-28 22:11:25 +0100

    added debug echo

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 # ensure, that the sync dir exists and is owned by the user
 [ -d $NC_SYNC_DIR ] || mkdir -p $NC_SYNC_DIR
+echo "chown $USER $NC_SOURCE_DIR"
 chown -R $USER $NC_SOURCE_DIR
 
 su $USER -c 'nextcloudcmd $( [ $NC_SILENT == true ] && echo "--silent" ) $( [ $NC_TRUST_CERT == true ] && echo "--trust" ) --non-interactive -u $NC_USER -p $NC_PASS $NC_SOURCE_DIR $NC_URL'
