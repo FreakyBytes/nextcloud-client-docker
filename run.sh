1ad8727f5a6b09a086f07efeae8de6d83bd82c36
Martin Peters <martin@freakybytes.net>
2017-12-29 13:59:10 +0100

    looping the sync command

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 
 # ensure, that the sync dir exists and is owned by the user
 [ -d $NC_SYNC_DIR ] || mkdir -p $NC_SYNC_DIR
-echo "chown $USER $NC_SOURCE_DIR"
-chown -R $USER $NC_SOURCE_DIR
+echo "chown $USER:$USER $NC_SOURCE_DIR"
+chown -R $USER:$USER $NC_SOURCE_DIR
 
-/bin/su $USER -c 'nextcloudcmd $( [ $NC_SILENT == true ] && echo "--silent" ) $( [ $NC_TRUST_CERT == true ] && echo "--trust" ) --non-interactive -u $NC_USER -p $NC_PASS $NC_SOURCE_DIR $NC_URL'
+while true
+do
+	/bin/su -s /bin/ash $USER -c 'nextcloudcmd $( [ $NC_SILENT == true ] && echo "--silent" ) $( [ $NC_TRUST_CERT == true ] && echo "--trust" ) --non-interactive -u $NC_USER -p $NC_PASS $NC_SOURCE_DIR $NC_URL'
+	sleep 300
+done
